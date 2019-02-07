e47c70d283637981ef24100a4aecda8ff5c355b3
Gaëtan <gaetanlagache@gmail.com>
2019-02-07 15:26:29 +0100

    add function on scroll2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 $(window).on("scroll", function(){
     var positionY = $(this).scrollTop();
-    console.log(positionY);
+    // console.log(positionY);
     if(positionY >= 1600){
         $("#scrollToTop").css("opacity", 1);
     }else if(positionY < 1600){
         $("#scrollToTop").css("opacity", 0);   
     }
 })
-$("#scrollToTop").click(function(){
-    $(window).scrollTop(0);
+    $("#scrollToTop").click(function(){
+        $(window).scrollTop(0);
+    })
+
+$(window).on("scroll", function(){
+        var positionY = $(this).scrollTop();
+    if(positionY > 100){
+        $("#navBar").css("padding", "0");
+    }else if(positionY < 99){
+        $("#navBar").css("padding", "20");
+    }
 })
