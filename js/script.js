f4b3e2637da6e34e3cbf7ecbed6dc8f19985a0e5
Gaëtan <gaetanlagache@gmail.com>
2019-02-07 15:00:44 +0100

    add jQuery function on scroll

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("Hello World");
+$(window).on("scroll", function(){
+    var positionY = $(this).scrollTop();
+    console.log(positionY);
+    if(positionY >= 1600){
+        $("#scrollToTop").css("opacity", 1);
+    }else if(positionY < 1600){
+        $("#scrollToTop").css("opacity", 0);   
+    }
+})
+$("#scrollToTop").click(function(){
+    $(window).scrollTop(0);
+})
