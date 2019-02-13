2d5cc9a6acd640e98f9c5856ed3150d929c5af2a
Gaëtan <gaetanlagache@gmail.com>
2019-02-13 11:20:37 +0100

    add portfolio

--- js/script.js
+++ js/script.js
@@ -56,3 +56,19 @@ $(".arrow").on("click", function(){
     }
 })
 //Slider JS
+
+//Portfolio
+$(function() {
+    var selectedClass = "";
+    $(".fil-cat").click(function(){ 
+    selectedClass = $(this).attr("data-rel"); 
+ $("#portfolio").fadeTo(100, 0.1);
+    $("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+setTimeout(function() {
+  $("."+selectedClass).fadeIn().addClass('scale-anm');
+  $("#portfolio").fadeTo(300, 1);
+}, 300); 
+    
+});
+});
+//Portfolio
